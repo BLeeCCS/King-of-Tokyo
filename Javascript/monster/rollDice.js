34bb3aab20b6c708627e4f61d2f0803144555716
SuperGato <ledbrandon@yahoo.com>
2020-01-08 21:21:12 -0800

    Fixed dice animation to play only on removed dice

--- Javascript/monster/rollDice.js
+++ Javascript/monster/rollDice.js
@@ -1,6 +1,5 @@
 export function roll(name,dice,count) {
     const diceSet = ["one","two","three","heart","energy","smash"];
-    let anotherDiceSet = [];
     let diceStart = null;
     let diceCount = 0;
 
@@ -11,60 +10,91 @@ export function roll(name,dice,count) {
         }
     }
 
-    for (let i = 0; i < dice.length; i++) {
-        anotherDiceSet[i] = dice[i];
-    }
-
     if (count > 1) {
-        for (let i = 0; i < dice.length; i++) {
+        for(let i = 0; i < dice.length; i++) {
             if (dice[i] === "" ) {
                 let random = Math.floor(Math.random(6) * 6 + 1);
                 dice[i] = diceSet[random-1];
+
+                let animation = setInterval(()=>{
+                    let random = Math.floor(Math.random(6) * 6);
+                    $(`#${name}_s > #diceContainer > :nth-child(${i+1})`).css({"background-image":"url(./assets/Dice/"+ diceSet[random] +".png)"});
+                },50);
+
+                setTimeout(() => {
+                    switch(dice[i]) {
+                        case "one":
+                            clearInterval(animation);
+                            $("#"+name+"_s > #diceContainer > :nth-child("+ (i+1) +")").css({"background-image":"url(./assets/Dice/one.png)"});
+                            break;
+                        case "two":
+                            clearInterval(animation);
+                            $("#"+name+"_s > #diceContainer > :nth-child("+ (i+1) +")").css({"background-image":"url(./assets/Dice/two.png)"});
+                            break;
+                        case "three":
+                            clearInterval(animation);
+                            $("#"+name+"_s > #diceContainer > :nth-child("+ (i+1) +")").css({"background-image":"url(./assets/Dice/three.png)"});
+                            break;
+                        case "heart":
+                            clearInterval(animation);
+                            $("#"+name+"_s > #diceContainer > :nth-child("+ (i+1) +")").css({"background-image":"url(./assets/Dice/heart.png)"});
+                            break;
+                        case "energy":
+                            clearInterval(animation);
+                            $("#"+name+"_s > #diceContainer > :nth-child("+ (i+1) +")").css({"background-image":"url(./assets/Dice/energy.png)"});
+                            break;
+                        case "smash":
+                            clearInterval(animation);
+                            $("#"+name+"_s > #diceContainer > :nth-child("+ (i+1) +")").css({"background-image":"url(./assets/Dice/smash.png)"});
+                            break;
+                    }
+                }, 500 * (i+1));
             }
         }
-    }
 
-    let animation = setInterval(()=>{
-        let random = Math.floor(Math.random(6) * 6);
-        $(`#${name}_s > #diceContainer > :nth-child(1)`).css({"background-image":"url(./assets/Dice/"+ diceSet[random] +".png)"});
-    },50);
-
-    diceStart = setInterval(() => {
-        switch(dice[diceCount]) {
-            case "one":
-                clearInterval(animation);
-                $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/one.png)"});
-                break;
-            case "two":
-                clearInterval(animation);
-                $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/two.png)"});
-                break;
-            case "three":
-                clearInterval(animation);
-                $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/three.png)"});
-                break;
-            case "heart":
-                clearInterval(animation);
-                $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/heart.png)"});
-                break;
-            case "energy":
-                clearInterval(animation);
-                $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/energy.png)"});
-                break;
-            case "smash":
-                clearInterval(animation);
-                $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/smash.png)"});
-                break;
-        }
-        diceCount++;
-
-        animation = setInterval(()=>{
+    } else {
+        let animation = setInterval(()=>{
             let random = Math.floor(Math.random(6) * 6);
-            $("#"+name+"_s > #diceContainer > :nth-child("+(diceCount+1)+")").css({"background-image":"url(./assets/Dice/"+ diceSet[random] +".png)"});
+            $(`#${name}_s > #diceContainer > :nth-child(1)`).css({"background-image":"url(./assets/Dice/"+ diceSet[random] +".png)"});
         },50);
-
-        if (diceCount == dice.length){
-            clearInterval(diceStart);
-        }
-    }, 500);
+    
+        diceStart = setInterval(() => {
+            switch(dice[diceCount]) {
+                case "one":
+                    clearInterval(animation);
+                    $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/one.png)"});
+                    break;
+                case "two":
+                    clearInterval(animation);
+                    $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/two.png)"});
+                    break;
+                case "three":
+                    clearInterval(animation);
+                    $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/three.png)"});
+                    break;
+                case "heart":
+                    clearInterval(animation);
+                    $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/heart.png)"});
+                    break;
+                case "energy":
+                    clearInterval(animation);
+                    $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/energy.png)"});
+                    break;
+                case "smash":
+                    clearInterval(animation);
+                    $("#"+name+"_s > #diceContainer > :nth-child("+ (diceCount+1) +")").css({"background-image":"url(./assets/Dice/smash.png)"});
+                    break;
+            }
+            diceCount++;
+    
+            animation = setInterval(()=>{
+                let random = Math.floor(Math.random(6) * 6);
+                $("#"+name+"_s > #diceContainer > :nth-child("+(diceCount+1)+")").css({"background-image":"url(./assets/Dice/"+ diceSet[random] +".png)"});
+            },50);
+    
+            if (diceCount == dice.length){
+                clearInterval(diceStart);
+            }
+        }, 500);
+    }
 }
